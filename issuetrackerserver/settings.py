c9b1469af6f85bb7ed92f0be19eaeedd74f4fc43
Syam Pradeep <syampradeep2b6@gmail.com>
2018-10-10 19:40:34 +0530

    added cors settings

--- issuetrackerserver/settings.py
+++ issuetrackerserver/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'issuetracker',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
